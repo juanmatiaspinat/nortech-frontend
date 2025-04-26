a409d192c78e5c60a0d39f8e8e4352450e269f42
federicocaballero <fedecaballero1234@gmail.com>
2025-04-26 17:18:42 -0300

    feat: modelos para funcionalidades

---
+++ src/models/crudProductos.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { API_URL_PRODUCTS } from "../api/conexiones";
+
+export async function MostrarProductos() {
+  const response = await axios.get(`${API_URL_PRODUCTS}/`);
+  return response.data;
+}
+
+export async function CrearProducto(producto, token) {
+  const response = await axios.post(
+    `${API_URL_PRODUCTS}/`,
+    producto,
+    {
+      headers: {
+        Authorization: `Bearer ${token}` 
+      }
+    }
+  );
+  return response.data;
+}
