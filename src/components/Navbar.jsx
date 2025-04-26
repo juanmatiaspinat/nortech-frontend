e79e1f147bb1dcb7a56f8cfa8b50faac34b47e6d
federicocaballero <fedecaballero1234@gmail.com>
2025-04-26 17:19:51 -0300

    feat: manejo condicional para mostrar informacion del usuario dependiendo de la autenticacion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import { Link } from 'react-router-dom';
-
-
+import { useAuthStore } from '../store/AuthStore';
+import { useNavigate } from 'react-router-dom';
 function Navbar() {
+    const { isAuthenticated, signout, datauserAuth } = useAuthStore();
+    console.log("🚀 ~ Navbar ~ datauserAuth:", datauserAuth)
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        signout();
+        navigate("/login");
+    };
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
@@ -17,9 +25,16 @@ function Navbar() {
                         <li className="nav-item">
                             <Link className="nav-link" to="/catalogo">Catálogo</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
+                        {isAuthenticated ? (
+                            <li className="nav-item">
+                                <span className="nav-link">{datauserAuth.email}</span>  {/* Mostrar el correo del usuario autenticado */}
+                                <button className="btn btn-link nav-link" onClick={handleLogout}>Logout</button>
+                            </li>
+                        ) : (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/login">Login</Link>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
