fae4aaa55d02d7ef0a8d65fbbedea9f4c8baa418
federicocaballero <fedecaballero1234@gmail.com>
2025-06-17 19:06:49 -0300

    feat: add admin edit link to ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,30 @@
-function ProductCard({ nombre, precio_venta, imagen }) {
-    return (
-        <div className="col-md-2 mb-2">
-            <div className="card h-100">
-                <img src={imagen} className="card-img-top" alt={nombre} />
-                <div className="card-body">
-                    <h5 className="card-title">{nombre}</h5>
-                    <p className="card-text">${precio_venta}</p>
-                    <button className="btn btn-primary w-100">Agregar al carrito</button>
-                </div>
-            </div>
+import { Link } from 'react-router-dom';
+
+function ProductCard({ nombre, precio_venta, imagen, id_producto, isAdmin }) {
+  return (
+    <div className="col-md-2 mb-2">
+      <div className="card h-100">
+        <img src={imagen} className="card-img-top" alt={nombre} />
+        <div className="card-body">
+          <h5 className="card-title">{nombre}</h5>
+          <p className="card-text">${precio_venta}</p>
+
+          <button className="btn btn-primary w-100">
+            Agregar al carrito
+          </button>
+
+          {isAdmin && (
+            <Link
+              className="btn btn-secondary w-100 mt-2"
+              to={`/admin/cargar-producto?id=${id_producto}&editar=true`}
+            >
+              Editar producto
+            </Link>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
+
 export default ProductCard;
